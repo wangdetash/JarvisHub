363b8711092e4595abec4d67ae2acdd0dced805a
athulantony <athul@entri.me>
2019-08-20 19:48:11 +0530

    fix issue in image rendering

--- WebServer.js
+++ WebServer.js
@@ -1,17 +1,27 @@
-
-var http = require('http');
 var fs = require('fs');
+var express=require('express');
+var path = require('path');
 
-const PORT=8080; 
 
-fs.readFile('./index.html', function (err, html) {
+var app = express();
+var dir = path.join(__dirname, 'public');
 
-    if (err) throw err;    
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
 
+const PORT=8080;
+
+app.get('/',(request,response)=>{
+  fs.readFile('./index.html', function (err, html) {
+      if (err) throw err;
+      response.writeHeader(200, {"Content-Type": "text/html"});
+      response.write(html);
+      response.end();
+  });
+
+})
+
+
+app.use(express.static(dir));
+app.listen(PORT,()=>{
+    console.log('server started')
+})
